092c671ca880a11459584918ce943081f30a0a81
akalamin <akalaminamin91@gmail.com>
2022-08-12 11:43:47 -0700

    right sidebar design almost done

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import Index from "./subComponents/LeftSidebar/Index";
 import DisplayPost from "./subComponents/mainContent/DisplayPost";
 import MainContent from "./subComponents/mainContent/MainContent";
-
+import RightSidebar from "./subComponents/RightSidebar/RightSidebar"
 const Home = () => {
   return (
     <>
       <main className="bg-home_bg p-0 md:p-6 font-poppins">
         {/* <h1>In the name of Allah</h1> */}
-        <div className="container grid grid-cols-8 gap-3">
+        <div className="container grid grid-cols-8 gap-6">
           <div className="col-span-2 hidden lg:block">
             <Index />
           </div>
@@ -16,7 +16,9 @@ const Home = () => {
             <MainContent />
             <DisplayPost />
           </div>
-          <div className="col-span-2 bg-white hidden lg:block"></div>
+          <div className="col-span-2 hidden lg:block">
+            <RightSidebar />
+          </div>
         </div>
       </main>
     </>
--- src/pages/Home/subComponents/RightSidebar/RightSidebar.js
+++ src/pages/Home/subComponents/RightSidebar/RightSidebar.js
@@ -1,7 +1,78 @@
 import React from "react";
+import { BiSearchAlt2 } from "react-icons/bi";
+import { BsThreeDots } from "react-icons/bs";
+import { RiVideoAddFill } from "react-icons/ri";
 
 const RightSidebar = () => {
-  return <div>RightSidebar</div>;
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <h2 className="text-gray-500 font-semibold text-lg mb-2">Contacts</h2>
+        <div className="flex items-center justify-between gap-4 text-lg">
+          <span
+            className="cursor-pointer p-2 hover:bg-gray-200 rounded-full"
+            title="New Room"
+          >
+            <RiVideoAddFill />
+          </span>
+          <span
+            className="cursor-pointer p-2 hover:bg-gray-200 rounded-full"
+            title="Search by name or group"
+          >
+            <BiSearchAlt2 />
+          </span>
+          <span
+            className="cursor-pointer p-2 hover:bg-gray-200 rounded-full"
+            title="Options"
+          >
+            <BsThreeDots />
+          </span>
+        </div>
+      </div>
+      <div className="flex items-start justify-center flex-col">
+        <div className="flex items-center justify-start gap-5 hover:bg-gray-200 w-full p-2 rounded-md cursor-pointer">
+          <img
+            className="w-[45px] h-[45px] rounded-full"
+            src="https://i.ibb.co/HCQ2MLL/profile.jpg"
+            alt="profile"
+          />
+          <h3 className="font-semibold text-sm">Md. Al Amin</h3>
+        </div>
+        <div className="flex items-center justify-start gap-5 hover:bg-gray-200 w-full p-2 rounded-md cursor-pointer">
+          <img
+            className="w-[45px] h-[45px] rounded-full"
+            src="https://i.ibb.co/HCQ2MLL/profile.jpg"
+            alt="profile"
+          />
+          <h3 className="font-semibold text-sm">Md. Al Amin</h3>
+        </div>
+        <div className="flex items-center justify-start gap-5 hover:bg-gray-200 w-full p-2 rounded-md cursor-pointer">
+          <img
+            className="w-[45px] h-[45px] rounded-full"
+            src="https://i.ibb.co/HCQ2MLL/profile.jpg"
+            alt="profile"
+          />
+          <h3 className="font-semibold text-sm">Md. Al Amin</h3>
+        </div>
+        <div className="flex items-center justify-start gap-5 hover:bg-gray-200 w-full p-2 rounded-md cursor-pointer">
+          <img
+            className="w-[45px] h-[45px] rounded-full"
+            src="https://i.ibb.co/HCQ2MLL/profile.jpg"
+            alt="profile"
+          />
+          <h3 className="font-semibold text-sm">Md. Al Amin</h3>
+        </div>
+        <div className="flex items-center justify-start gap-5 hover:bg-gray-200 w-full p-2 rounded-md cursor-pointer">
+          <img
+            className="w-[45px] h-[45px] rounded-full"
+            src="https://i.ibb.co/HCQ2MLL/profile.jpg"
+            alt="profile"
+          />
+          <h3 className="font-semibold text-sm">Md. Al Amin</h3>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RightSidebar;
