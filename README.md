3455b2b2d0fe597b8f923b3f69c91c36132d3131
akalamin <akalaminamin91@gmail.com>
2022-08-12 23:30:34 -0700

    update readme file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Show code screenshot
 ![Code 1](https://i.ibb.co/BPqnLGD/code-1.png)
+![Code 2](https://i.ibb.co/7j8LMZ5/comment.png)
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { useState } from "react";
 
 const SignUp = () => {
+  const [userInfo, setUserInfo] = useState({});
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
   return (
     <>
       <div class="p-20 md:h-[95vh] h-auto bg-gray-200">
@@ -12,20 +21,29 @@ const SignUp = () => {
             </p>
           </div>
           <div class="container mx-auto flex flex-col items-center">
-            <form class="shadow-lg min-w-[360px] p-4 flex flex-col bg-white rounded-lg">
+            <form
+              onSubmit={handleSubmit}
+              class="shadow-lg min-w-[360px] p-4 flex flex-col bg-white rounded-lg"
+            >
               <input
                 type="text"
                 placeholder="Your name"
+                name="name"
+                onChange={handleChange}
                 class="mb-3 py-3 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-blue-500"
               />
               <input
                 type="email"
                 placeholder="Email "
+                name="email"
+                onChange={handleChange}
                 class="mb-3 py-3 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-blue-500"
               />
               <input
                 type="password"
                 placeholder="Password"
+                name="password"
+                onChange={handleChange}
                 class="mb-3 py-3 px-4 border border-gray-400 focus:outline-none rounded-md focus:ring-1 ring-blue-500"
               />
               <button class="w-full bg-blue-500 text-white p-3 rounded-lg font-semibold text-lg">
